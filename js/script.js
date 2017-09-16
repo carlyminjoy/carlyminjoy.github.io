2933be634fb1a6650159eb1babe7a1a1ae1377ab
Carly Minjoy <cminjoy@myune.edu.au>
2017-09-16 17:56:10 +1000

    Added gotcha config to html for formspree

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
 
     $('#contact-form').submit(function(e) {
+        e.preventDefault();
         $.ajax({
             method: 'POST',
             url: 'https://formspree.io/carlylminjoy@gmail.com',
             data: $(this).serialize(),
             datatype: 'json'
         });
-        e.preventDefault();
-        $(this).get(0).reset();
         alertify.success('Message sent!');
+        $(this).get(0).reset();
     });
 
     $('.nav-btn').click(function(evt) {
