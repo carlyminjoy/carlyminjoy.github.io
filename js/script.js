7dafa41f8b034cfbd4f59e99ebeb50636fd2c651
Carly Minjoy <cminjoy@myune.edu.au>
2017-09-16 16:27:26 +1000

    Added alertify notifications & js/ajax post function for contact form

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         var message = $('inputMessage');
 
         if (!name.value || !email.value || !message.value) {
-          // alertify.error 'Please check your entries'
+            alertify.error('Please check your entries.');
             return false;
         } else {
             $.ajax({
@@ -15,9 +15,9 @@ $(document).ready(function () {
                 data: $(this).serialize(),
                 datatype: 'json'
             });
-        // e.preventDefault();
+        e.preventDefault();
         $(this).get(0).reset();
-        // alertify.success 'Message sent'
+        alertify.success('Message sent!');
         }
     });
 
