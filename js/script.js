ad2e01409ec261b9fdb49d38fd7a5b1807857bf7
Carly Minjoy <cminjoy@myune.edu.au>
2017-09-16 16:56:08 +1000

    added alertify js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,15 @@
 $(document).ready(function () {
 
     $('#contact-form').submit(function(e) {
-        var name    = $('.inputName');
-        var email   = $('.inputEmail');
-        var message = $('.inputMessage');
-
-        if (!name.value || !email.value || !message.value) {
-            alertify.error('Please check your entries.');
-            return false;
-        } else {
-            $.ajax({
-                method: 'POST',
-                url: '//formspree.io/carlylminjoy@gmail.com',
-                data: $(this).serialize(),
-                datatype: 'json'
-            });
+        $.ajax({
+            method: 'POST',
+            url: '//formspree.io/carlylminjoy@gmail.com',
+            data: $(this).serialize(),
+            datatype: 'json'
+        });
         e.preventDefault();
         $(this).get(0).reset();
         alertify.success('Message sent!');
-        }
     });
 
     $('.nav-btn').click(function(evt) {
