05b0cea54bc2bcd4573b7b51acd3fdceb1b8b86e
Carly Minjoy <cminjoy@myune.edu.au>
2017-09-16 16:20:50 +1000

    added scroll functionality

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
-$(document).ready {
-  $('#contact-form').submit(function(e) {
-    var name    = $('inputName');
-    var email   = $('inputEmail');
-    var message = $('inputMessage');
+$(document).ready(function () {
 
-    if (!name.value || !email.value || !message.value) {
-      // alertify.error 'Please check your entries'
-      return false;
-    } else {
-      $.ajax {
-        method: 'POST',
-        url: '//formspree.io/carlylminjoy@gmail.com',
-        data: $(this).serialize(),
-        datatype: 'json'
-      }
-      e.preventDefault();
-      $(this).get(0).reset();
-      // alertify.success 'Message sent'
-    }
-}
+    $('form').submit(function(e) {
+        var name    = $('inputName');
+        var email   = $('inputEmail');
+        var message = $('inputMessage');
+
+        if (!name.value || !email.value || !message.value) {
+          // alertify.error 'Please check your entries'
+            return false;
+        } else {
+            $.ajax({
+                method: 'POST',
+                url: '//formspree.io/carlylminjoy@gmail.com',
+                data: $(this).serialize(),
+                datatype: 'json'
+            });
+        // e.preventDefault();
+        $(this).get(0).reset();
+        // alertify.success 'Message sent'
+        }
+    });
+
+    $('.nav-btn').click(function(evt) {
+        var navHeight = $(".navbar").height();
+        evt.preventDefault();
+        console.log('clicked!');
+        $('html, body').stop().animate({
+            scrollTop: $( $(this).attr('href') ).offset().top - navHeight
+        }, 1000);
+    });
+});
