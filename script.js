9808e0a1d852873ce702f80e7e0d2a8a3d943566
Carly Minjoy <cminjoy@myune.edu.au>
2018-11-18 00:37:52 +1000

    new portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    // scrolling nav buttons
+    $('.nav-item').click(function(evt) {
+    var navHeight = $(".navbar").height();
+    evt.preventDefault();
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - navHeight
+    }, 500);
+    });
+});
