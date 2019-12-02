2b7026da38010478447981a414c92eef160ed223
Carly Minjoy <cminjoy@myune.edu.au>
2019-12-02 18:44:31 +1000

    smooth scroll

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const nav = document.getElementById('nav');
+const links = ['about', 'projects', 'contact'];
+
 document.addEventListener('scroll', function(e) {
-    var nav = document.getElementById('nav');
     if (window.scrollY == 0) {
         nav.classList.add('transparent')
         nav.classList.remove('shadow');
@@ -7,6 +9,20 @@ document.addEventListener('scroll', function(e) {
         nav.classList.remove('transparent');
         nav.classList.add('shadow');
     }
+})
 
-    console.log(window.scrollY);
+links.forEach(function(link) {
+    let linkId = link + '-link';
+    let linkEl = document.getElementById(linkId);
+
+    console.log('linkel', linkEl)
+    
+    linkEl.addEventListener('click', function(e) {
+        e.preventDefault();
+        console.log('inside event listener', link)
+
+        document.getElementById(link).scrollIntoView({ 
+            behavior: 'smooth' 
+        });
+    })
 })
